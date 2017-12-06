8884079ca1e9ffaf77195c04fd3342f04cd6dabd
Trevor <guitaraddict97@yahoo.com>
2017-12-06 00:02:30 -0600

    Revision 1

--- MainCode.c
+++ MainCode.c
@@ -1,25 +1,35 @@
 #include <REG932.h>
 #include <Simon2.h>
+#include <MusicLib.h>
 
-float quarter;
-float eighth;
-float half;
-float whole;
-unsigned char bpm;
-unsigned char mode;
-int cycles;
-int complement;
+unsigned char TH0_saved;
+unsigned char TH1_saved;
+unsigned char TL0_saved;
+unsigned char TL1_saved;
 
-void delay_t0(float ms);
-void calc_bpm(unsigned char bpm);	
+void init_shortdelay(unsigned int us, unsigned char timer);
+void init_longdelay(unsigned int us, unsigned char timer);
+void cont_longdelay(unsigned char timer);
+void sound(unsigned short* song, unsigned short bpm);
 
+void timer0() interrupt 1 {
+	Speaker = ~Speaker;
+	TR0 = 0;
+	TF0 = 0;
+	TL0 = TL0_saved;
+	TH0 = TH0_saved;
+	TR0 = 1;
+}
+	
 void main() {
-	mode = 0;
-	calc_bpm(200);
-
+	unsigned char mode = 0;
+	unsigned short bpm = 200;
+	
 	while(1) {
-		if(SW7 == 0)
+		if(SW7 == 0) {
 			mode = 1;
+			sound(POKEMON, bpm);
+		}
 		else if(SW8 == 0)
 			mode = 2;
 		else if(SW9 == 0)
@@ -28,26 +38,87 @@ void main() {
 }
 
 
-void delay_t0(float ms) { 
-	//Works up to 71 ms
+void sound(unsigned short* song, unsigned short bpm) {
+	//Both timers are in mode 1
+	//Timer 0 controls tones, while 1 controls spacing
+	//Timer 0 done with interrupts, 1 done manually
+	unsigned int eighth = (30/bpm)*1000000;
+	unsigned short index = 0;
+	unsigned short length = sizeof(song)/sizeof(short);
+	unsigned short i;
+	unsigned char j;
 	
-	cycles = (ms*1000)/1.085;
-	TMOD &= 0xF0;
-	TMOD |= 0x01;
+	TMOD = 0x11;
+	EA = 1;
+	ET0 = 1;
 	
-	complement = 0xFFFF-cycles+1;
-	TH0 = complement >> 4;
-	TL0 = complement && 0x0F;
-	TR0 = 1;
+	init_longdelay(eighth,1);
 	
-	while (TF0 == 0)
-	TR0 = 0;
-	TF0 = 0;
+	for(i=0; i+=2; i<length) {
+		init_longdelay(song[i],0);
+		TR0 = 0;
+
+		for(j=0; j+=1; j<song[i+1]) {
+			cont_longdelay(1);
+		}
+	}
 }
 
-void calc_bpm(unsigned char bpm) {
-	quarter = 60 / bpm;
-	eighth = quarter / 2;
-	half = quarter * 2;
-	whole = half * 2;
+void init_longdelay(unsigned int us, unsigned char timer) { 
+	//Works up to 71000 us
+	short cycles;
+	short complement;
+	cycles = us/1.085;
+	complement = 0xFFFF-cycles+1;
+	
+	if(timer == 0) {
+		TR0 = 0;
+		TF0 = 0;
+		TH0_saved = complement >> 4;
+		TL0_saved = complement && 0x0F;
+	}
+	else {
+		TR1 = 0;
+		TF1 = 0;
+		TH1_saved = complement >> 4;
+		TL1_saved = complement && 0x0F;
+	}
+}
+
+void cont_longdelay(unsigned char timer) {
+	if(timer == 0) {
+		TR0 = 0;
+		TF0 = 0;
+		TH0 = TH0_saved;
+		TL0 = TL0_saved;
+		TR0 = 1;
+		while(TF0 == 0)
+		TR0 = 0;
+		TF0 = 0;
+	}
+	else {
+		TR1 = 0;
+		TF1 = 0;
+		TH1 = TH1_saved;
+		TL1 = TL1_saved;
+		TR1 = 1;
+		while(TF1 == 0)
+		TR1 = 0;
+		TF1 = 0;
+	}
+}
+
+void init_shortdelay(unsigned int us, unsigned char timer) {
+	//Works up to 138 us
+	short cycles;
+	cycles = us/1.085;
+	
+	if(timer == 0) {
+		TL0 = 0;
+		TH0 = -cycles;
+	}
+	else {
+		TL1 = 0;
+		TH1 = -cycles;
+	}
 }
---
+++ Music_library.h
@@ -0,0 +1,129 @@
+// notes defined in us
+#ifndef __MUSICLIB_H__
+#define __MUSICLIB_H__
+
+	#define rest 0
+	#define Ab0 19260
+	#define A0 18182
+	#define Bb0 17159
+	#define B0 16197
+	#define C0 30581
+	#define Db0 28868
+	#define D0 27248
+	#define Eb0 25707
+	#define E0 24272
+	#define F0 22904
+	#define Gb0 21626
+	#define G0 20408
+	#define Ab1 (Ab0/2)
+	#define A1 (A0/2)
+	#define Bb1 (Bb0/2)
+	#define B1 (B0/2)
+	#define C1 (C0/2)
+	#define Db1 (Db0/2)
+	#define D1 (D0/2)
+	#define Eb1 (Eb0/2)
+	#define E1 (E0/2)
+	#define F1 (F0/2)
+	#define Gb1 (Gb0/2)
+	#define G1 (G0/2)
+	#define Ab2 (Ab0/4)
+	#define A2 (A0/4)
+	#define Bb2 (Bb0/4)
+	#define B2 (B0/4)
+	#define C2 (C0/4)
+	#define Db2 (Db0/4)
+	#define D2 (D0/4)
+	#define Eb2 (Eb0/4)
+	#define E2 (E0/4)
+	#define F2 (F0/4)
+	#define Gb2 (Gb0/4)
+	#define G2 (G0/4)
+	#define Ab3 (Ab0/8)
+	#define A3 (A0/8)
+	#define Bb3 (Bb0/8)
+	#define B3 (B0/8)
+	#define C3 (C0/8)
+	#define Db3 (Db0/8)
+	#define D3 (D0/8)
+	#define Eb3 (Eb0/8)
+	#define E3 (E0/8)
+	#define F3 (F0/8)
+	#define Gb3 (Gb0/8)
+	#define G3 (G0/8)
+	#define Ab4 (Ab0/16)
+	#define A4 (A0/16)
+	#define Bb4 (Bb0/16)
+	#define B4 (B0/16)
+	#define C4 (C0/16)
+	#define Db4 (Db0/16)
+	#define D4 (D0/16)
+	#define Eb4 (Eb0/16)
+	#define E4 (E0/16)
+	#define F4 (F0/16)
+	#define Gb4 (Gb0/16)
+	#define G4 (G0/16)
+	#define Ab5 (Ab0/32)
+	#define A5 (A0/32)
+	#define Bb5 (Bb0/32)
+	#define B5 (B0/32)
+	#define C5 (C0/32)
+	#define Db5 (Db0/32)
+	#define D5 (D0/32)
+	#define Eb5 (Eb0/32)
+	#define E5 (E0/32)
+	#define F5 (F0/32)
+	#define Gb5 (Gb0/32)
+	#define G5 (G0/32)
+	#define Ab6 (Ab0/64)
+	#define A6 (A0/64)
+	#define Bb6 (Bb0/64)
+	#define B6 (B0/64)
+	#define C6 (C0/64)
+	#define Db6 (Db0/64)
+	#define D6 (D0/64)
+	#define Eb6 (Eb0/64)
+	#define E6 (E0/64)
+	#define F6 (F0/64)
+	#define Gb6 (Gb0/64)
+	#define G6 (G0/64)
+	#define Ab7 (Ab0/128)
+	#define A7 (A0/128)
+	#define Bb7 (Bb0/128)
+	#define B7 (B0/128)
+	#define C7 (C0/128)
+	#define Db7 (Db0/128)
+	#define D7 (D0/128)
+	#define Eb7 (Eb0/128)
+	#define E7 (E0/128)
+	#define F7 (F0/128)
+	#define Gb7 (Gb0/128)
+	#define G7 (G0/128)
+	#define Ab8 (Ab0/256)
+	#define A8 (A0/256)
+	#define Bb8 (Bb0/256)
+	#define B8 (B0/256)
+	#define C8 (C0/256)
+	#define Db8 (Db0/256)
+	#define D8 (D0/256)
+	#define Eb8 (Eb0/256)
+	#define E8 (E0/256)
+	#define F8 (F0/256)
+	#define Gb8 (Gb0/256)
+	#define G8 (G0/256)
+	
+//Songs defined in note then length
+//Length 1 corresponds to eighth note
+	
+code unsigned short POKEMON[] = {
+	A5, 1,
+	B7, 2,
+	C6, 3,
+	rest, 3,
+	C5, 2,
+	A5, 10,
+	rest, 4,
+	A4, 1
+}
+
+#endif
